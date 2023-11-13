9f2856ade9bd231990d9e24d8fa48d7e16ee001d
zaynlannister <zaynlannister@gmail.com>
2023-11-13 12:02:42 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <p>some text</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
